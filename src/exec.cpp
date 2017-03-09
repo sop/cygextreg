c23aa2545ebc4d42043cb77234b5a7f16bb03eb5
Joni Eskelinen <joni@mobie.fi>
2017-03-09 12:57:09 +0200

    Fix charset issues
    * add documentation

--- src/exec.cpp
+++ src/exec.cpp
@@ -4,6 +4,7 @@
 #include <sstream>
 #include <algorithm>
 #include <sys/cygwin.h>
+#include <sys/stat.h>
 #include <windows.h>
 #include <shlwapi.h>
 #include "strconv.hpp"
@@ -24,39 +25,31 @@ int ExecCommand::run() {
 	if (!_fileExists(script_path)) {
 		throw std::runtime_error("Script not found.");
 	}
-	std::vector<std::wstring> newargs;
+	std::wstringstream cmd_line;
 	/* Windows path to mintty.exe */
 	if (0 != cygwin_conv_path(
 			CCP_POSIX_TO_WIN_W, "/bin/mintty.exe", w_pathbuf,
 			sizeof(w_pathbuf))) {
 		throw std::runtime_error(strerror(errno));
 	}
-	newargs.push_back(std::wstring(w_pathbuf));
-	/* force MinTTY's charset to UTF-8 */
-	newargs.push_back(L"-o");
-	newargs.push_back(L"Charset=UTF-8");
-	/* window title */
-	newargs.push_back(L"-t");
-	newargs.push_back(script_name);
-	/* execute in bash interactive session */
-	newargs.push_back(L"--exec");
-	newargs.push_back(L"/bin/sh");
-	newargs.push_back(L"-il");
-	newargs.push_back(L"--");
+	cmd_line << w_pathbuf << L" -o Charset=UTF-8"
+	         << L" -t " << _escapeArg(script_name)
+	         << L" --exec /bin/sh -il --";
 	for (auto arg : _getExecArgs()) {
-		/* convert Windows paths to cygwin form */
+		/* convert Windows paths to Cygwin form */
 		if (_isWinPath(arg, true)) {
-			newargs.push_back(_pathWinToPosix(arg));
-		} else {
-			newargs.push_back(arg);
+			arg = mb_to_wide(_pathWinToPosix(arg));
 		}
+		cmd_line << L" " << _escapeArg(arg);
 	}
-	return _execute(newargs);
+	_execute(cmd_line.str());
+	return 0;
 }
 
 std::vector<std::wstring> ExecCommand::_getExecArgs() {
 	std::vector<std::wstring> args;
 	size_t i;
+	/* search beginning of exec arguments */
 	for (i = 0; i < _args.size(); ++i) {
 		if (_args[i] == L"--") {
 			break;
@@ -68,22 +61,17 @@ std::vector<std::wstring> ExecCommand::_getExecArgs() {
 	return args;
 }
 
-int ExecCommand::_execute(std::vector<std::wstring> args) {
-	STARTUPINFOA si = {};
+void ExecCommand::_execute(std::wstring cmd_line) {
+	STARTUPINFO si = {};
 	si.cb = sizeof(si);
 	PROCESS_INFORMATION pi = {};
+	/* set PATH environment variable */
 	env::EnvVar(L"PATH").set(_getEnvPath());
-	std::wstringstream cmd_args;
-	for(auto arg : args) {
-		cmd_args << "\"" << _escapeArg(arg) << "\" ";
-	}
-	std::string cmd_argsa = wide_to_mb(cmd_args.str(), CP_UTF8);
-	if (0 == CreateProcessA(
-			NULL, (LPSTR)cmd_argsa.c_str(), NULL, NULL, FALSE,
+	if (0 == CreateProcess(
+			NULL, (LPWSTR)cmd_line.c_str(), NULL, NULL, FALSE,
 			CREATE_NEW_PROCESS_GROUP, NULL, NULL, &si, &pi)) {
 		THROW_LAST_ERROR("Failed to start process");
 	}
-	return 0;
 }
 
 bool ExecCommand::_isWinPath(std::wstring path, bool must_exist) {
@@ -99,22 +87,29 @@ bool ExecCommand::_isWinPath(std::wstring path, bool must_exist) {
 }
 
 bool ExecCommand::_fileExists(std::wstring path) {
-	return TRUE == PathFileExists(path.c_str());
+	std::string posix_path = _pathWinToPosix(path);
+	struct stat s;
+	if (-1 == stat(posix_path.c_str(), &s)) {
+		return false;
+	}
+	if (!(S_ISREG(s.st_mode) || S_ISLNK(s.st_mode))) {
+		return false;
+	}
+	return true;
 }
 
-std::wstring ExecCommand::_pathWinToPosix(std::wstring winpath) {
+std::string ExecCommand::_pathWinToPosix(std::wstring winpath) {
 	char buf[MAX_PATH + 1];
 	if (0 != cygwin_conv_path(
 			CCP_WIN_W_TO_POSIX, winpath.c_str(), buf, sizeof(buf))) {
 		throw std::runtime_error(strerror(errno));
 	}
-	/* Cygwin represents paths in utf-8 */
-	return mb_to_wide(buf, CP_UTF8);
+	return std::string(buf);
 }
 
 std::wstring ExecCommand::_escapeArg(std::wstring arg) {
 	_replaceAll(arg, L"\"", L"\\\"");
-	return arg;
+	return arg.insert(0, L"\"").append(L"\"");
 }
 
 void ExecCommand::_replaceAll(std::wstring& str, const std::wstring& from,
--- src/exec.hpp
+++ src/exec.hpp
@@ -14,15 +14,76 @@ public:
 	}
 	int run();
 private:
+	/**
+	 * Get the arguments for script execution.
+	 *
+	 * @return A list of wstring arguments
+	 */
 	std::vector<std::wstring> _getExecArgs();
-	int _execute(std::vector<std::wstring> args);
+
+	/**
+	 * Execute script using a given command line.
+	 *
+	 * @param std::wstring cmd_line Command line
+	 * @throws std::runtime_error on failure
+	 */
+	void _execute(std::wstring cmd_line);
+
+	/**
+	 * Check whether given path exists and is a regular file or symlink.
+	 *
+	 * @param std::wstring path Path
+	 * @return bool True if file exists
+	 */
 	bool _fileExists(std::wstring path);
+
+	/**
+	 * Check whether given path is in Windows format.
+	 *
+	 * @param std::wstring path Path
+	 * @param bool must_exist Whether file must exist
+	 * @return bool True if path is in Windows format
+	 */
 	bool _isWinPath(std::wstring path, bool must_exist);
-	std::wstring _pathWinToPosix(std::wstring winpath);
+
+	/**
+	 * Convert Windows path to POSIX format.
+	 *
+	 * @param std::wstring winpath Windows formatted path
+	 * @return std::string POSIX path in UTF-8 charset
+	 */
+	std::string _pathWinToPosix(std::wstring winpath);
+
+	/**
+	 * Escape command line argument with Windows semantics.
+	 *
+	 * @param std::wstring arg Argument
+	 * @return std::wstring Escaped argument
+	 */
 	std::wstring _escapeArg(std::wstring arg);
+
+	/**
+	 * Replace all occurences of a string.
+	 *
+	 * @param std::wstring& str String to modify
+	 * @param const std::wstring& from Substring to search
+	 * @param const std::wstring& to Replacement string
+	 */
 	void _replaceAll(std::wstring& str, const std::wstring& from,
 	                 const std::wstring& to);
+
+	/**
+	 * Get PATH environment variable for to be set before executing script.
+	 *
+	 * @return std::wstring
+	 */
 	std::wstring _getEnvPath();
+
+	/**
+	 * Get readable script name for the shell window title.
+	 *
+	 * @return std::wstring
+	 */
 	std::wstring _getScriptName(std::wstring path);
 };
 
